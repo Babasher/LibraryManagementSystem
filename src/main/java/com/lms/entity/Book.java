8a26d29a422e1e5bf44e4c80adac7ee6cfa2938e
Omar <omohammed7869@gmail.com>
2021-12-17 10:56:43 -0600

    created data type for Address and Person, created Author Book and Library Entity classes

---
+++ src/main/java/com/lms/entity/Book.java
@@ -0,0 +1,48 @@
+package com.lms.entity;
+
+public class Book {
+	private String ISBN;
+	private String title;
+	private String subject;
+	private String publisher;
+	private String language;
+	private int numberOfPages;
+	
+	public String getISBN() {
+		return ISBN;
+	}
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	public int getNumberOfPages() {
+		return numberOfPages;
+	}
+	public void setNumberOfPages(int numberOfPages) {
+		this.numberOfPages = numberOfPages;
+	}
+	
+}
---
+++ src/main/java/com/lms/entity/Library.java
@@ -0,0 +1,21 @@
+package com.lms.entity;
+
+import com.lms.dataType.Address;
+
+public class Library {
+	private String name;
+	private Address address;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+}
